20dae3f025975eb64c172ed3840befafd3ca80cd
Aqib Bala <73388336+aqibmehrajbala@users.noreply.github.com>
2021-07-02 16:31:42 +0530

    server conf

--- britx/settings.py
+++ britx/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-htklxm@g)c2(+z^%vk0-un0&_#dwb@vx-9-#fohm^+y#57_x&u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['134.122.119.182']
 
 
 # Application definition
@@ -82,10 +82,21 @@ WSGI_APPLICATION = 'britx.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'britxdb',
+        'USER': 'rafiq',
+        'PASSWORD': 'arif4730',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
